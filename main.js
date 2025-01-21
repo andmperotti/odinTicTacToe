610febe96cecca493cbe4f66e2af7a49d72a6e12
Andrew Perotti <Perotti28@gmail.com>
2025-01-21 12:21:21 -0800

    Add functionality for PlayGame variables, function
    For our PlayGame to work correctly we need to create placeholders for certain values like how many moves have been made so we can stop the game when the board is full, nextPlayerMove so we can keep track of who's turn it is next, players object so we have specific values saved and easily accessible for when a player sets themselves in a Cell.
    I have just started the function to ask a player for a move but need to go back and change the array to remain just an array of elements and not an array of arrays of elements as it will make checking for winning combinations way easier.

--- main.js
+++ main.js
@@ -23,12 +23,28 @@ function Cell(){
     //an instance of the GameBoard is imported and used
     //players object holds the players and their name and marker
     //game play logic:
-        //prompt ask /tell the player to choose where they want to place their marker, if already taken tell them its taken and ask them again
         //use a variable to keep track of who's turn it is next
         //use a variable to keep track of how many moves there have been, as there can be ties and we want to end the game in case of that later
+        //prompt ask /tell the player to choose where they want to place their marker, if already taken tell them its taken and ask them again
         //check for a winner each turn, by parsing over cells and looking for specific combinations of cell values
         //If there is a winner stop the playing and declare a winner, in html you can add a class to highlight the cells of the winning placements. Otherwise keep playing the game
+let PlayGame = function(){
+    let game = GameBoard
+    const players = [{
+        name: 'player 1', 
+        marker: 'X'
+    },{
+        name: 'player 2', 
+        marker: 'O'
+    }]
+    let moves = 0
+    let nextPlayerMove = players[0]
 
+    function askPlayerMove(place){
+        if()
+    }
+}        
+PlayGame()
 //DisplayGame will be inside an iife as there will only be one game to display
     //At first this will just output to the console
     //Later we can render html elements using this

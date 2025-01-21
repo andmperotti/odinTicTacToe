26035f902f6ae05635d502187c753ea9e471d517
Andrew Perotti <Perotti28@gmail.com>
2025-01-21 14:31:04 -0800

    Add checkWinner, change other functions
    Made too many changes sadly, fixing one problem branched into fixing many others.

--- main.js
+++ main.js
@@ -33,17 +33,70 @@ let PlayGame = function(){
     }]
     let moves = 0
     let nextPlayerMove = players[0]
-    function printBoard(board){
+    let winner = false
+
+    function changePlayer(){
+        nextPlayerMove===players[0] ? nextPlayerMove=players[1] : nextPlayerMove=players[0]
+    }
+    function printBoard(){
         console.log(`${board[0].getValue()} ${board[1].getValue()} ${board[2].getValue()}`)
         console.log(`${board[3].getValue()} ${board[4].getValue()} ${board[5].getValue()}`)
         console.log(`${board[6].getValue()} ${board[7].getValue()} ${board[8].getValue()}`)
     }
-    printBoard(board)
-    // function askPlayerMove(place){
-    //     if(board[place].value!==''){
+    function askPlayerMove(player){
+        let place = prompt(`What position, ${nextPlayerMove.name} would you like to take? `)
+        if(board[place].getValue()==='_'){
+            board[place].setValue(nextPlayerMove.marker)
+        }else{
+            askPlayerMove(player)
+        }
+        moves++
+        checkWinner()
+        changePlayer()
+    }
+    while(moves<10&&!winner){
+        printBoard()
+        askPlayerMove(nextPlayerMove)
+    }
 
-    //     }
-    // }
+    function checkWinner(){
+        if( //player possible wins
+            //horizontals
+            (board[0].getValue()==='X'&&board[1].getValue()==='X'&&board[2].getValue()==='X')||
+            (board[3].getValue()==='X'&&board[4].getValue()==='X'&&board[5].getValue()==='X')||
+            (board[6].getValue()==='X'&&board[7].getValue()==='X'&&board[8].getValue()==='X') ||
+            //verticals
+            (board[0].getValue()==='X'&&board[3].getValue()==='X'&&board[6].getValue()==='X')||
+            (board[1].getValue()==='X'&&board[4].getValue()==='X'&&board[7].getValue()==='X')||
+            (board[2].getValue()==='X'&&board[5].getValue()==='X'&&board[8].getValue()==='X') ||
+            //diagonals
+            (board[0].getValue()==='X'&&board[4].getValue()==='X'&&board[8].getValue()==='X') ||
+            (board[6].getValue()==='X'&&board[4].getValue()==='X'&&board[2].getValue()==='X')
+        ){
+            winner = true;
+            printBoard()
+            //temporary line of code because it's not dynamic at all
+            console.log(`player 1 wins!`)
+
+        }else if(
+            (board[0].getValue()==='O'&&board[1].getValue()==='O'&&board[2].getValue()==='O')||
+            (board[3].getValue()==='O'&&board[4].getValue()==='O'&&board[5].getValue()==='O')||
+            (board[6].getValue()==='O'&&board[7].getValue()==='O'&&board[8].getValue()==='O') ||
+            //verticals
+            (board[0].getValue()==='O'&&board[3].getValue()==='O'&&board[6].getValue()==='O')||
+            (board[1].getValue()==='O'&&board[4].getValue()==='O'&&board[7].getValue()==='O')||
+            (board[2].getValue()==='O'&&board[5].getValue()==='O'&&board[8].getValue()==='O') ||
+            //diagonals
+            (board[0].getValue()==='O'&&board[4].getValue()==='O'&&board[8].getValue()==='O') ||
+            (board[6].getValue()==='O'&&board[4].getValue()==='O'&&board[2].getValue()==='O')
+        ){
+            winner=true
+            printBoard()
+            console.log(`player 2 wins!`)
+        }else if(!winner&&moves>9){ //tie game
+            console.log('Tie!')
+        }
+    }
 }        
 PlayGame()
 //DisplayGame will be inside an iife as there will only be one game to display

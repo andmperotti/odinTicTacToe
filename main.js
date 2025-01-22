ba38b4d863f5790d692f5726dec7ea49acbb8dbe
Andrew Perotti <Perotti28@gmail.com>
2025-01-22 11:27:12 -0800

    Omit starter pseudo code, add results explanation
    As the console version of this is finished I have removed the wordy pseudocode and left comments where I think some explanation is needed.

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-//GameBoard object, only one is needed so iife it
-    //board array, stores cells of playing board
-        //Cells should be instances of an object that has a value and maybe methods to change and access that value (value, getValue, setValue)
 let GameBoard = (function(){
     let board = new Array(9)
     for(let i = 0; i<board.length; i++){
@@ -14,15 +11,6 @@ let GameBoard = (function(){
     return {board}
 })()
 
-//PlayGame will be an object, but inside an iife because we only want one game to be playing
-    //an instance of the GameBoard is imported and used
-    //players object holds the players and their name and marker
-    //game play logic:
-        //use a variable to keep track of who's turn it is next
-        //use a variable to keep track of how many moves there have been, as there can be ties and we want to end the game in case of that later
-        //prompt ask /tell the player to choose where they want to place their marker, if already taken tell them its taken and ask them again
-        //check for a winner each turn, by parsing over cells and looking for specific combinations of cell values
-        //If there is a winner stop the playing and declare a winner, in html you can add a class to highlight the cells of the winning placements. Otherwise keep playing the game
 let PlayGame = function(){
     let board= GameBoard.board
     const players = [
@@ -67,7 +55,7 @@ let PlayGame = function(){
     }
 
     function checkWinner(){
-        if( //player possible wins
+        if( //player 1 possible winning combinations
             //horizontals
             (board[0].getValue()==='X'&&board[1].getValue()==='X'&&board[2].getValue()==='X')||
             (board[3].getValue()==='X'&&board[4].getValue()==='X'&&board[5].getValue()==='X')||
@@ -82,10 +70,10 @@ let PlayGame = function(){
         ){
             winner = true;
             printBoard()
-            //temporary line of code because it's not dynamic at all
             console.log(`player 1 wins!`)
 
         }else if(
+            //player 2 possible winning combinations
             (board[0].getValue()==='O'&&board[1].getValue()==='O'&&board[2].getValue()==='O')||
             (board[3].getValue()==='O'&&board[4].getValue()==='O'&&board[5].getValue()==='O')||
             (board[6].getValue()==='O'&&board[7].getValue()==='O'&&board[8].getValue()==='O') ||
@@ -100,14 +88,10 @@ let PlayGame = function(){
             winner=true
             printBoard()
             console.log(`player 2 wins!`)
-        }else if(!winner&&moves===9){ //tie game
+        }else if(!winner&&moves===9){ 
+            //tie game
             console.log('Tie!')
         }
     }
 }        
 PlayGame()
-//DisplayGame will be inside an iife as there will only be one game to display
-    //At first this will just output to the console
-    //Later we can render html elements using this
-
-    //We want to console.log a text representation of our games board
